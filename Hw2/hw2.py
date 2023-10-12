ef500d4e83d1ce27f883422cd4916a2b6ee7476e
Pitipat-PC <pitipatw@mit.edu>
2023-10-12 08:44:09 -0400

    update hw2

--- Hw2/hw2.py
+++ Hw2/hw2.py
@@ -12,7 +12,7 @@ ps.set_use_prefs_file(False)
 ps.init() # only call once
 
 # Load triangle mesh
-vertices, faces = gpy.read_mesh("Hw2/armadillo.obj")
+vertices, faces = gpy.read_mesh("armadillo.obj")
 ps_mesh0 = ps.register_surface_mesh("Rest mesh", vertices, faces, transparency=0.1) # display mesh
 ps_mesh = ps.register_surface_mesh("Deformed mesh", vertices, faces) # display mesh
 nv = vertices.shape[0] # number of vertices
@@ -28,7 +28,11 @@ col = np.concatenate((faces[:,1],faces[:,2],faces[:,0]))
 A3 = np.concatenate((A,A,A))
 W = sp.sparse.csr_matrix((A3, (row,col)),shape=(nv,nv))
 W = W + W.T
-
+# print("W")
+# print(W)
+# print("---")
+# print(W[(0,3)])
+# print("END W")
 # Homework problem 3c:
 #    Input:
 #        restPositions     --- nv x 3 matrix of vertex positions before deformation (p)
@@ -38,8 +42,25 @@ W = W + W.T
 #    Hint:  check out numpy.linalg.svd(...)
 def findARAPRotations(restPositions,deformedPositions):
     R = np.zeros((nv,3,3))
-
+    
     ##### ANSWER GOES HERE #####
+    for i in range(nv):
+        pi = restPositions[i,:]
+        pi_prime = deformedPositions[i,:]
+        eij = pi - restPositions
+        eij_prime = pi_prime - deformedPositions
+
+        Pi = eij
+        Pi_prime = eij_prime
+        #construct Di
+        Di = np.zeros((nv,nv))
+        for j in range(nv): 
+            Di[j,j] = W[(i,j)]
+
+        Si = Pi*Di*Pi_prime
+# perform singular value decomposition Si = UiViT
+        U,S,Vh =  np.linalg.svd(Si)
+        R[i,:,:] = U*Vh.T
     
     ##### END ANSWER #####
 
