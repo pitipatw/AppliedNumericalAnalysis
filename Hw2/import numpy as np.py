37e36c1d7c9ce67e9112460e9f3cbae5d90836bc
pitipat_mac <pitipatw@mit.edu>
2023-10-13 06:46:05 -0400

    update hw2 done

--- Hw2/import numpy as np.py
+++ Hw2/import numpy as np.py
@@ -1,9 +1,8 @@
 import numpy as np
+import scipy as sp
 
-
-A = np.array([[ 1,2,3], [4,5,6]])
+A = np.array([1,2,3,4,5,6])
+print(A[[2,1,3]])
+B= np.delete(A,[0,1,2])
 print(A)
-B = np.array([10,30,40])\
-
-C = A - B
-print(C)
+print(np.insert(A, [0,1,2], [0,1,2], axis=0))
